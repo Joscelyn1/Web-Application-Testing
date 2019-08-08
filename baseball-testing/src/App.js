787f4f25cd1371e5a423c6b109b6e1761731d40a
Joscelyn <JoscelynRose@gmail.com>
2019-08-08 16:49:30 -0400

    set up basic app

--- baseball-testing/src/App.js
+++ baseball-testing/src/App.js
@@ -1,32 +1,37 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 import Dashboard from "./components/Dashboard.js";
 import Display from "./components/Display.js";
 
 function App() {
-  const [balls, setBalls] = useState(0);
-  const [strikes, setStrikes] = useState(0);
+  let [balls, setBalls] = useState(0);
+  let [strikes, setStrikes] = useState(0);
 
   function addBalls() {
+    if (balls === 3) {
+      setBalls(-1);
+    }
     setBalls(balls => balls + 1);
   }
 
   function addStrikes() {
+    if (strikes === 2) {
+      setStrikes(-1);
+    }
     setStrikes(strikes => strikes + 1);
+  }
+
+  function setToZero() {
+    setBalls(0);
+    setStrikes(0);
   }
 
   return (
     <div className="App">
-      <Display
-        balls={balls}
-        strikes={strikes}
-        addBalls={addBalls}
-        addStrikes={addStrikes}
-      />
+      <Display balls={balls} strikes={strikes} />
       <Dashboard
-        balls={balls}
-        strikes={strikes}
+        setToZero={setToZero}
         addBalls={addBalls}
         addStrikes={addStrikes}
       />
--- baseball-testing/src/components/Display.js
+++ baseball-testing/src/components/Display.js
@@ -1,1 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Container, Row, Col } from "shards-react";
+function Display(props) {
+  return (
+    <Container className="displayContainer">
+      <Row>
+        <Col>
+          <h4>Balls</h4>
+          <h4>{props.balls}</h4>
+        </Col>
+        <Col>
+          <h4>Strikes</h4>
+          <h4>{props.strikes}</h4>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Display;
